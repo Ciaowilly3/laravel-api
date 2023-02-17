d5f9d1a716c7670a23051946a20fab758c46677c
Ciaowilly3 <muscomassi@gmail.com>
2023-02-17 16:42:03 +0100

    Added some features

--- app/Http/Controllers/Api/ProjectController.php
+++ app/Http/Controllers/Api/ProjectController.php
@@ -8,9 +8,19 @@ use Illuminate\Http\Request;
 
 class ProjectController extends Controller
 {
-    public function index(){
-        $projects = Project::all();
-
+    public function index(Request $request){
+        $last4 = $request->input("last4");
+        if ($last4){
+            $projects = Project::with("technologies", "type")->orderBy('created_at', 'DESC')->limit(4)->get();
+        }
+        else{
+            $projects = Project::with("technologies", "type")->get();
+        }
         return response()->json($projects);
+    }
+    public function show($id) {
+        $project = Project::findOrFail($id);
+
+        return response()->json($project);
     }
 }
